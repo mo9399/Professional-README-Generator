d20779ca5f40ac0c060c564f581b8860bc28b1e3
your name <you@example.com>
2022-05-19 14:43:57 -0700

    add README.md content

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Professional-README-Generator
+## Description
+
+A command-line application that generates a professional README.md file from a user's input using the Inquire package.
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+- Node.js
+- Inquirer
+
+## Installation
+
+1. Clone the repo using `git clone`
+2. Navigate to the app directory
+3. Run `npm install` to install dependencies
+4. Run `node index.js` in the command line to launch the application
+
+## Usage
+
+![Video demonstration of how to use the app](media/professional-README-generator.gif)
+
+## Contribution
+
+Made by Mohamed Abdullahi
--- index.js
+++ index.js
@@ -1,39 +1,36 @@
-// Packages needed for this application
+// Packages for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// Array of questions for user input
+// Array of questions 
 const questions = [
-  // Name
   {
     type: "input",
     name: "title",
-    message: "What is the title of the project?",
+    message: "What is the name of your project?",
     validate: (titleInput) => {
       if (titleInput) {
         return true;
       } else {
-        console.log("Please enter the project title!");
+        console.log("Please enter the project Name!");
         return false;
       }
     },
   },
-  // Description
   {
     type: "input",
     name: "description",
-    message: "Provide a description of the project.",
+    message: "Please provide a description of your project.",
     validate: (descriptionInput) => {
       if (descriptionInput) {
         return true;
       } else {
-        console.log("Please provide a project description!");
+        console.log("Please enter a project description!");
         return false;
       }
     },
   },
-  // Installation
   {
     type: "input",
     name: "installation",
@@ -42,70 +39,62 @@ const questions = [
       if (installationInput) {
         return true;
       } else {
-        console.log("Please provide installation instructions!");
+        console.log("Please enter installation instructions!");
         return false;
       }
     },
   },
-  // Usage
   {
     type: "input",
     name: "usage",
-    message: "Provide instructions and examples for use.",
+    message: "Provide instructions for use.",
     validate: (usageInput) => {
       if (usageInput) {
         return true;
       } else {
-        console.log("Please provide usage information!");
+        console.log("Please enter usage information!");
         return false;
       }
     },
   },
-  // How To Contribute
   {
     type: "input",
     name: "contribution",
-    message: "Provide guidelines for how to contribute to the project.",
+    message: "Guidelines for how to contribute to the project.",
     validate: (contributionInput) => {
       if (contributionInput) {
         return true;
       } else {
-        console.log("Please provide guidelines for contributing!");
+        console.log("Please enter guidelines for contributing!");
         return false;
       }
     },
   },
-  // Tests
   {
     type: "input",
     name: "tests",
-    message: "Provide instructions on how to test this project.",
+    message: "Instructions on how to test this project.",
     validate: (testsInput) => {
       if (testsInput) {
         return true;
       } else {
-        console.log("Please provide test instructions!");
+        console.log("Please enter test instructions!");
         return false;
       }
     },
   },
-  // License
   {
     type: "list",
     name: "license",
     message: "Select the license for your project.",
     choices: [
-      "GNU AGPLv3",
       "GNU GPLv3",
-      "GNU LGPLv3",
-      "Mozilla",
       "MIT",
-      "Apache",
-      "Boost",
+      "Apache 2.0",
+      "BSD 3",
       "No license",
     ],
   },
-  // Github Username
   {
     type: "input",
     name: "github",
@@ -119,7 +108,6 @@ const questions = [
       }
     },
   },
-  // Email Address
   {
     type: "input",
     name: "email",
@@ -152,7 +140,7 @@ const writeToFile = (fileContent) => {
   });
 };
 
-// Function to initialize app
+// Function to Initialize app
 const init = () => {
   return inquirer.prompt(questions).then((readmeData) => {
     return readmeData;
@@ -174,3 +162,4 @@ init()
   .catch((err) => {
     console.log(err);
   });
+
